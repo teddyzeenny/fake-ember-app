88e026a7ad6ac9975738fb1f4e8d04b1b269e557
Teddy Zeenny <teddyzeenny@gmail.com>
2013-11-07 20:29:28 +0200

    redirect to post

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
 var App = window.App = Ember.Application.create();
 
 App.Router.map(function() {
-  this.route('post');
+  this.route('posts');
 });
 
-App.ApplicationRoute = Ember.Route.extend({
-  model: function() {
-    return Ember.RSVP.Promise(function(resolve) {
-      Ember.run.later(null, resolve, 1000);
-    }, "Delay application route");
+App.ApplicationRoute = Ember.Route.extend();
+
+App.IndexRoute = Ember.Route.extend({
+  setupController: function() {
+    Ember.run.later(this, function() {
+      this.transitionTo('posts');
+    }, 500);
   }
 });
 
-App.IndexRoute = Ember.Route.extend({
+App.PostRoute = Ember.Route.extend({
   setupController: function() {
     var p = new Ember.RSVP.resolve('[<App.User:1>, <App.User:2>]', "Find Users");
     p.then(function() {
       throw "HTTP STATUS 404 - NOT FOUND";
     }, null, "Find Comments");
-  },
-
-  model: function() {
-    return Ember.RSVP.Promise(function(resolve) {
-      Ember.run.later(null, resolve, 1000);
-    }, "Model Promise");
   }
 });
