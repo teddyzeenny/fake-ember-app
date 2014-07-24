80f65894ac62329ad168b26a8a0e5bcbf785003d
Teddy Zeenny <teddyzeenny@gmail.com>
2014-07-24 15:09:41 +0300

    iframes

--- app.js
+++ app.js
@@ -6,18 +6,63 @@ setTimeout(function() {
   App.advanceReadiness();
 }, 1000);
 
-App.IndexController = Ember.Controller.extend({
-  isActive: true,
+window.a = [{
+  name: 1,
+  done: false,
+  num: 1
+}, {
+  name: 2,
+  done: false,
+  num: 2
+},
+ {
+  name: 3,
+  done: true,
+  num: 5
+}];
 
-  items: function() {
-    var a = [];
-    // for (var i = 0; i < 1000; i++ ){
-    //   a.push(i);
-    // }
-    return a;
-  }.property()
+
+App.IndexController = Ember.Controller.extend({
+
+  search: '',
+
+  chores: a,
+
+  remaining: Ember.computed.filter('chores', function(item) {
+    if (item.done) { return false; }
+    return item.name === (+this.get('search')) || Ember.isEmpty(this.get('search'));
+  }).property('chores.@each.num', 'chores.@each.done', 'search'),
+
+  // remaining: a
+
+  // remaining: Ember.arrayComputed('chores.@each.done', 'chores.@each.num', {
+  //   addedItem: function(array, item) {
+  //     console.log('added', item.name);
+  //     if (!item.done) {
+  //       array.pushObject(item);
+  //     }
+  //     return array;
+  //   },
+  //   removedItem: function(array, item) {
+  //     console.log('removed', item.name);
+  //     array.removeObject(item);
+  //     return array;
+  //   }
+  // })
 });
 
+
+
+App.Post = DS.Model.extend({
+  title: DS.attr('string'),
+  body: DS.attr('body')
+});
+
+App.Post.reopenClass({
+  FIXTURES: []
+});
+
+App.ApplicationAdapter = DS.FixtureAdapter;
 
 App.IndexView = Ember.View.extend({
 
